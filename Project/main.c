3a4a432759e336f7928a0f41ebdba8c1febf93c9
Songjeongwoo <wjddn0728@naver.com>
2017-11-18 17:17:49 +0900

    qsort alorithm change, Debugging and if rank ==0) no print

--- Project/main.c
+++ Project/main.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
 
 #include <stdio.h>
 #include <string.h>
@@ -13,14 +14,14 @@ typedef struct __Filedata {
 	char * Filename;
 }Filedata;
 
-int compare(void * first, void * second) {
-	if (((Filedata *)first) -> rank < ((Filedata *)second)->rank)
-		return 1;
-	else if (((Filedata *)first)->rank > ((Filedata *)second)->rank)
-		return -1;
-	else
-		return 0; 
+int compare(void * first, void * second) 
+{
+	if (((Filedata*)first)->rank == ((Filedata*)second)->rank) {
+		return strcmp(((Filedata*)first)->Filename, ((Filedata*)second)->Filename);
+	}
+	return ((Filedata*)first)->rank > ((Filedata*)second)->rank ? -1 : 1;
 }
+
 
 int main(int argc, char * argv[])
 {
@@ -30,13 +31,18 @@ int main(int argc, char * argv[])
 	clock_t a=clock();
 	long handle;
 	int result = 1;
-	int filelength, cnt = 0, fnamelength;
+	int filelength, cnt = 0, fnamelength, filecnt =0;
 	char * keywordplacepointer;
 	char * filestr;
+	char b[50000] = "";
 
 	handle = _findfirst("./*.txt", &folderdata);
-	if (handle == -1) return 0;
-	while (result != -1) {
+	if (handle == -1) {
+		printf("There is no file");
+		return 0;
+	}
+	while (result != -1) 
+	{
 		result = _findnext(handle, &folderdata);
 		cnt++;
 	}
@@ -45,13 +51,12 @@ int main(int argc, char * argv[])
 
 	handle = _findfirst("./*.txt", &folderdata);
 	
-	for (int i = 0; i < cnt; i++) {
-
+	for (int i = 0; i < cnt; i++) 
+	{
 		fd[i].rank = 0;
 		fnamelength = strlen(folderdata.name);
 		fd[i].Filename = (char*)malloc((fnamelength+1) * sizeof(char));
 		strcpy(fd[i].Filename, folderdata.name);
-		result = _findnext(handle, &folderdata);
 		
 		fin = fopen(folderdata.name, "r");
 		fseek(fin, 0, SEEK_END);
@@ -60,6 +65,7 @@ int main(int argc, char * argv[])
 		filestr = (char*)malloc((filelength+1) * sizeof(char));
 		fread(filestr, 1, filelength, fin);
 		filestr[filelength] = '\0';
+		filestr = strlwr(filestr);
 		keywordplacepointer = filestr;
 		while (keywordplacepointer != 0) {
 			keywordplacepointer = strstr(keywordplacepointer, argv[1]);
@@ -84,15 +90,25 @@ int main(int argc, char * argv[])
 			}
 		}
 		fclose(fin);
+		result = _findnext(handle, &folderdata);
 	}
 	_findclose(handle);
 
 	qsort(fd, cnt, sizeof(Filedata), compare);
-	char b[50000]="";
+
 	for (int i = 0; i < cnt; i++) {
-		sprintf(b,"%s Rank : %d / 파일명 : %s \n",b, fd[i].rank, fd[i].Filename);
+		if (fd[i].rank == 0) {
+			filecnt++;
+		}
+	}
+	cnt -= filecnt; 
+
+	for (int i = 0; i < cnt; i++) {
+		sprintf(b, "%s Rank : %d / 파일명 : %s \n", b, fd[i].rank, fd[i].Filename);
 	}
 	printf("%s", b);
+	printf("\nfilecnt : %d, cnt: %d \n\n", filecnt, cnt);
+
 	printf("%2.5lf", ((double)clock() - a) / CLOCKS_PER_SEC);
 	system("pause");
 	return 0;
